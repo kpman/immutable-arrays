c4bfd8d446a73ef187f06839c608bd7946f2b3af
Daniel Tseng <s92f002@hotmail.com>
2018-04-03 16:08:18 +0800

    Change del, splice implement (#1)

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const pop = x => x.slice(0, -1);
 const unshift = y => x => [y, ...x];
 const shift = x => x.slice(1);
 const sort = f => x => [...x].sort(f);
-const del = i => x => [...x.slice(0, i), ...x.slice(i + 1)];
-const splice = (s, c, ...y) => x => [...x.slice(0, s), ...y, ...x.slice(s + c)];
+const del = i => x => [...x].splice(i, 1);
+const splice = (i, len) => x => [...x].splice(i, len);
 
 export { clone, push, pop, unshift, shift, sort, del, splice };
